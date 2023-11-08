b7ceb5504c232f9f5e3da0b31e3d354c9b341a13
root <aksiubat@gmail.com>
2023-11-08 09:24:13 +0000

    first commit

---
+++ server/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Add the NodeSource repository for Node.js 16
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+echo "deb-src [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+
+# Update package list
+sudo apt update
+
+# Install Node.js 16
+sudo apt install nodejs
+
